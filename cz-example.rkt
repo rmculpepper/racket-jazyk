b05fa757624bd6857740264e898391bd6a394d06
Ryan Culpepper <ryanc@racket-lang.org>
2019-03-08 11:57:37 +0100

    ... cz-example

---
+++ cz-example.rkt
@@ -0,0 +1,20 @@
+#lang jazyk
+
+#:definitions
+(require "language/cz.rkt")
+
+#:section "Examples"
+
+#:kind translation
+"Mám otazku." = "I have a question."
+"Dobrý den."  = "Good day."
+
+#:kind noun-phrase
+"imperfektní sloveso"		= "imperfective verb"
+"perfektní sloveso"		= "perfective verb"
+
+#:kind adjective
+dobrý = good
+hezký = pretty
+inteligentní = intelligent
+moderní = modern
